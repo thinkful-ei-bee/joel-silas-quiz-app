b7ee8b9bfc3fe30bf63668011017320d2021c9a7
Silas <silasishallahan@gmail.com>
2019-02-26 11:31:11 -0800

    stubbing out funcs - Silas

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const STORE = {
+  
+};
+
+const QUESTIONS = [];
+
+/*
+Render the start page.
+Go to the first question page,
+which displays question 1.
+Record user answer, including index of question,
+and whether the user was correct / incorrect.
+Reload the question page with the next question.
+Repeat this for all 5 questions.
+Go to the result page.
+Display the results.
+Optionally restart the game
+*/
+
+function renderStartPage(){
+  //window.location.href = 
+}
+
+function renderQuestionPage(){
+
+}
+
+function renderResultPage(){
+
+}
+
+function handleUserAnswer(){
+
+}
+
+function main(){
+  renderStartPage();
+  renderQuestionPage();
+  renderResultPage();
+  handleUserAnswer();
+}
+
+$(main);
